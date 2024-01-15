d5dc1c0acd24465d57bb3f89905b6a3e7ca02274
Walace <walacecastro@live.com>
2024-01-15 17:07:16 -0300

    Abreviação de Operadores Aritméticos

---
+++ Fundamentos/AbreviacaoOperadoresAritmeticos.java
@@ -0,0 +1,36 @@
+package Fundamentos;
+
+public class AbreviacaoOperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        int quantidadeProdutos = 10;
+
+        //Adicionando mais produtos
+
+        quantidadeProdutos = quantidadeProdutos + 1;
+        quantidadeProdutos = quantidadeProdutos - 1;
+        quantidadeProdutos = quantidadeProdutos / 2;
+        quantidadeProdutos = quantidadeProdutos * 2;
+
+        //Ou
+
+        quantidadeProdutos += 1;
+        quantidadeProdutos -= 1;
+        quantidadeProdutos *= 2;
+        quantidadeProdutos /= 2;
+        quantidadeProdutos %= 2;
+
+
+
+
+        System.out.println(quantidadeProdutos);
+
+
+
+
+
+
+    }
+
+}
