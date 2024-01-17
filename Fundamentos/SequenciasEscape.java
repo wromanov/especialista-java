676f1b94748d86cdaab9cc8f174052b85d59a078
Walace <walacecastro@live.com>
2024-01-17 17:33:33 -0300

    Sequência de Escape.

---
+++ Fundamentos/SequenciasEscape.java
@@ -0,0 +1,20 @@
+package Fundamentos;
+
+public class SequenciasEscape {
+    public static void main(String[] args) {
+
+        //Usasse "\" para escapar as duplas.
+        System.out.println("Oi \"Maria\"");
+
+        //Pular linha "\n"
+        System.out.print("Ola meus\namigos!\n");
+
+        //Escapar a "barra" usa-se duas barras.
+        System.out.println("C:\\Windows\\System");
+
+
+
+
+
+    }
+}
