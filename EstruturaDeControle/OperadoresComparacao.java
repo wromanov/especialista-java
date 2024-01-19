d86555d35d4c24deac639495bba8f2aa1f6e9d97
walacedelgado <walacecastro@live.com>
2024-01-19 18:30:16 -0300

    Operadores de comparação

---
+++ EstruturaDeControle/OperadoresComparacao.java
@@ -0,0 +1,21 @@
+package EstruturaDeControle;
+
+public class OperadoresComparacao {
+    public static void main(String[] args) {
+        int pesoProdutos = 900;
+        int limitePesoCaminhao = 1000;
+
+        boolean cargaExcedida = pesoProdutos > limitePesoCaminhao;
+        boolean cargaLiberada = pesoProdutos <= limitePesoCaminhao;
+
+        System.out.printf("Carga Excedida %b.\n", cargaExcedida);
+
+        System.out.printf("Carga Liberada %b.\n", cargaLiberada);
+
+
+
+
+    }
+
+
+}
