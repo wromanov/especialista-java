369538ab9efdbbdc1ac1a07cac968cf91070989c
walacedelgado <walacecastro@live.com>
2024-01-23 13:37:08 -0300

    Operadores Lógicos

---
+++ EstruturaDeControle/OperadorLogico.java
@@ -0,0 +1,38 @@
+package EstruturaDeControle;
+
+import java.util.Scanner;
+
+public class OperadorLogico {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cortisol: ");
+        double cortisol = sc.nextDouble();
+
+        // Cortisol Ideal entre 6 a 18.4
+        boolean cortisolNormal = cortisol >= 6 && cortisol <=18.4; // Operador && (and) - Todas as condições devem ser verdadeiras, para o resultado ser verdadeiro.
+        //boolean cortisolAnormal = !(cortisol >= 6 && cortisol <= 18.4); // Negação - Nega o resultado, invertendo o valor.
+        boolean cortisolAnormal = cortisol < 6 || cortisol > 18.4; // Operador || (or) - Pelo menos uma condição deve ser verdadeira, para o resultado ser verdadeiro.
+
+        System.out.printf("Cortisol normal: %b %n", cortisolNormal);
+        System.out.printf("Cortisol Anormal: %b %n", cortisolAnormal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
