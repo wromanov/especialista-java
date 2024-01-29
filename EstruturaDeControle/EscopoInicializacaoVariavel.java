55a9475edebf7631b08c9d57f5a6ceb85de49b27
Walace Delgado <walacecastro@live.com>
2024-01-29 13:17:26 -0300

    Escopo de Inicialização de Variável

---
+++ EstruturaDeControle/EscopoInicializacaoVariavel.java
@@ -0,0 +1,28 @@
+package EstruturaDeControle;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class EscopoInicializacaoVariavel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Infome o preço do produto: ");
+        double precoProduto = sc.nextDouble();
+
+        System.out.print("Cobrar Frete: ");
+        boolean cobrarFrete = sc.nextBoolean();
+
+        double valorFrete = 0; //Todas os tipos primitivos devem ser inicializados com um valor.
+
+        if (cobrarFrete) {
+            System.out.printf("Valor do Frete: ");
+            valorFrete = sc.nextDouble(); //Variável de escopo local
+        }
+
+        double valorTotal = precoProduto + valorFrete; //Variável de escopo global
+
+        System.out.printf("Valor total: R$%.2f%n", valorTotal);
+
+    }
+}
