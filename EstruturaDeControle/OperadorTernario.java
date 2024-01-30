baacd7ceb86b337f1f6896326dd9020abaa375be
Walace Delgado <walacecastro@live.com>
2024-01-30 12:48:52 -0300

    Desafio Operador Ternário

---
+++ EstruturaDeControle/OperadorTernario.java
@@ -0,0 +1,27 @@
+package EstruturaDeControle;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        char tipoNotaFiscal = 'P';
+        double totalFaturado = 1200.30;
+
+//        double valorImpostos;
+//
+//        if (tipoNotaFiscal == 'S') {
+//            valorImpostos = totalFaturado * 0.16;
+//        } else {
+//            valorImpostos = totalFaturado * 0.35;
+//        }
+
+
+        //double valorImpostos = tipoNotaFiscal == 'S' ? totalFaturado * 0.16 : totalFaturado * 0.35;
+
+        double taxaImpostos = tipoNotaFiscal == 'S' ? 0.16 : 0.35;
+        double valorImpostos = totalFaturado * taxaImpostos;
+
+        System.out.printf("Total Faturado: R$%.2f%n", totalFaturado);
+        System.out.printf("Valor dos impostos: R$%.2f%n", valorImpostos);
+
+    }
+
+}
